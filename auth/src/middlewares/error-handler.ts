2be61f55f4ef5db922803b966232c6183e1c826a
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-11-16 19:42:53 +0100

    Update middleware

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,14 +1,14 @@
-import {ErrorRequestHandler, Request, Response, NextFunction} from 'express';
+import {Request, Response, NextFunction} from 'express';
 import {CustomError} from '../errors/custom-error';
 
-export const errorHandler: ErrorRequestHandler = (
+export const errorHandler = (
     err: Error,
     req: Request,
     res: Response,
     next: NextFunction
 ) => {
     if (err instanceof CustomError) {
-        res.status(err.statusCode).send({errors: err.serializeErrors()});
+        return res.status(err.statusCode).send({errors: err.serializeErrors()});
     }
 
     res.status(400).send({
