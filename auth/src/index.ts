849ab4020b06b8a1e951a7516a903da27630db02
Habeeb Kareem <olaidekareem43@yahoo.com>
2024-11-16 19:38:50 +0100

    Move app startup code to app.js

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,41 +1,10 @@
-import express from 'express';
-import 'express-async-errors';
-import {json} from 'body-parser';
 import mongoose from 'mongoose';
-import cookieSession from 'cookie-session';
-
-import {currentUserRouter} from './routes/current-user';
-import {signinRouter} from './routes/signin';
-import {signoutRouter} from './routes/signout';
-import {signupRouter} from './routes/signup';
-import {NotFoundError} from './errors/not-found-error';
-import {errorHandler} from './middlewares/error-handler';
-
-const app = express();
-app.set('trust proxy', true); // traffic is being proxied to the app through NGINX
-app.use(json());
-app.use(
-    cookieSession({
-        signed: false,
-        secure: true,
-    })
-);
-
-app.use(currentUserRouter);
-app.use(signupRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-
-app.all('*', async (req, res) => {
-    throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import {app} from './app';
 
 const start = async () => {
     // check if env variable is defined
     if (!process.env.JWT_KEY) {
-        throw new Error('JWT_KEY must be defined!')
+        throw new Error('JWT_KEY must be defined!');
     }
 
     try {
